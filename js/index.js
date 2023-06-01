f2d4720d1628a91be8952c92f7939e1a4d351689
NicoEtche2004 <netchepare@udesa.edu.ar>
2023-05-31 21:43:53 -0300

    boton dark mode anda

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let darkmode = document.querySelector('.botondarkmode');
+let body = document.body;
+let lightmode = 'light';
+
+darkmode.addEventListener('click', function(){
+
+    body.style.backgroundColor = "black"
+
+})

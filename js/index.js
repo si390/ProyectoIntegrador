bf886ea97aad3d7a8706fda8ee330b0e031ac2c4
si390 <soyolaiberico@udesa.edu.ar>
2023-06-04 16:58:20 -0300

    solve conflicts

--- js/index.js
+++ js/index.js
@@ -85,3 +85,12 @@ fetch(urlsongs)
 
 
 
+let darkmode = document.querySelector('.botondarkmode');
+let body = document.body;
+let lightmode = 'light';
+
+darkmode.addEventListener('click', function(){
+
+    body.style.backgroundColor = "black"
+
+})
